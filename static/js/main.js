ef41a9e193dfc6c3f27ef9bcb5a36860125b6a43
Jacob Knispel <knispeja@rose-hulman.edu>
2016-11-16 03:15:41 -0500

    Made website look a lot nicer, only thing left is TF stuff

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,34 @@
-$(document).ready(function() {  
+$(document).ready(function() {
+    $("#upscaled").toggle();
     new Dropzone(document.body, { // Make the whole body a dropzone
         url: "/api",
+        thumbnailWidth: null,
+        thumbnailHeight: null,
         previewsContainer: "#previews",
-        clickable: "#clickable",
         success: function(file, response) {
-            window.location = response;
+            // Hide some Dropzone elements
+            $("div.dz-success-mark").remove();
+            $("div.dz-error-mark").remove();
+            $("div.dz-details").remove();
+            $("div.dz-progress").remove();
+
+            // Add image and make it full-size
+            $("dz-image").css({"width":"100%", "height":"auto"});
+            document.getElementById("upscaled").src=response;
+        },
+        accept: function(file, done) {
+            console.log("uploaded");
+            done();
+        },
+        init: function() {
+            this.on("addedfile", function() {
+                if (this.files[1]!=null){
+                    this.removeFile(this.files[0]);
+                } else {
+                    $("#instructions").toggle();
+                    $("#upscaled").toggle();
+                }
+            });
         }
     });
 });
